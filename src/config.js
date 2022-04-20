f5dc0ec2f00a6d931b9bb71f4e582b94dd7fdcf6
b-oreilly <berniceoreilly@gmail.com>
2022-04-20 21:18:54 +0100

    base URL changed to hosted backend

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
 axios.defaults.withCredentials = true
-axios.defaults.baseURL = 'http://localhost:8080';
+axios.defaults.baseURL = 'https://wecycle-backend.herokuapp.com/';
 
 export default axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "https://wecycle-backend.herokuapp.com/",
      headers: {
          Authorization: `Bearer ${localStorage.getItem('token')}`
      }
