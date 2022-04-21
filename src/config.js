f332f29caebec9992cae25472145f76bcddc6169
b-oreilly <berniceoreilly@gmail.com>
2022-04-21 10:26:40 +0100

    .

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
 axios.defaults.withCredentials = true
-axios.defaults.baseURL = 'https://wecycle-backend.herokuapp.com/';
+axios.defaults.baseURL = 'https://wecycle-backend.herokuapp.com';
 
 export default axios.create({
-    baseURL: "https://wecycle-backend.herokuapp.com/",
+    baseURL: "https://wecycle-backend.herokuapp.com",
      headers: {
          Authorization: `Bearer ${localStorage.getItem('token')}`
      }
