d819e55cff2c8f7698d0d9d9357c6bb521badd5f
b-oreilly <berniceoreilly@gmail.com>
2022-04-25 09:38:47 +0100

    About page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,10 +4,24 @@
       <v-row no-gutters align="center" class="mt-4">
         <v-col>
           <div class="d-flex justify-center col">
-            <h1>About</h1>
+            <v-card flat>
+              <h1>About</h1>
+              <v-card-text class="about">
+                Too often are good items of food, clothing, furniture, etc. thrown away as they're deemed as waste.<br>
+                The purpose of the application is to enable users to share their unwanted items with other users in
+                their area, <br> also for businesses to share their items no longer fit for sale that would otherwise be disposed
+                of.
+              </v-card-text>
+            </v-card>
           </div>
         </v-col>
       </v-row>
     </div>
   </v-container>
 </template>
+
+<style scoped>
+ .about {
+   font-size: 16px;
+ }
+</style>
